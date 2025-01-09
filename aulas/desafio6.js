78f335efafa580b0be1be7fc6e4a35bf37075c31
Pedro Renato Binelo Briltes <159266176+pedrobinelo@users.noreply.github.com>
2025-01-09 12:20:31 -0400

    Update desafio6.js

--- aulas/desafio6.js
+++ aulas/desafio6.js
@@ -23,8 +23,25 @@ class Person {
     calculateImc () {
         return this.weight / (Math.pow(this.height, 2));
     }
+
+    classifyImc () {
+        const imc = this.calculateImc();
+        if (imc < 18.5) {
+            return 'Underweight';
+        } else if (imc >= 18.5 && imc < 25) {
+            return 'Normal weight';
+        } else if (imc >= 25 && imc < 30) {
+            return 'Overweight';
+        } else if (imc >= 30 && imc < 40) {
+            return 'Obese';
+        } else {
+            return 'Morbid obesity';
+        }
+    }
+
 }
 
 const jose = new Person('José', 70, 1.75);
 console.log(jose);
 console.log(jose.calculateImc().toFixed(2));
+console.log(jose.classifyImc());
