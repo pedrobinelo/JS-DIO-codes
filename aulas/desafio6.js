2d27637aba3fe2cfd921b532644c2b18933cdbeb
Pedro Renato Binelo Briltes <159266176+pedrobinelo@users.noreply.github.com>
2025-01-09 12:03:43 -0400

    Create desafio6.js

---
+++ aulas/desafio6.js
@@ -0,0 +1,30 @@
+/* 
+    Create a class to represent a person.
+
+    Each person has a name, weight and height.
+
+    Each person has the capacity of saying it's own IMC value. (IMC = weight / (height * height))
+
+    Instantiate a person named José, who weighs 70kg and is 1.75m tall. Ask José to show his IMC 
+    value.
+*/
+
+class Person {
+    name;
+    weight;
+    height;
+
+    constructor (name, weight, height) {
+        this.name = name; 
+        this.weight = weight; 
+        this.height = height; 
+    }
+
+    calculateImc () {
+        return this.weight / (Math.pow(this.height, 2));
+    }
+}
+
+const jose = new Person('José', 70, 1.75);
+console.log(jose);
+console.log(jose.calculateImc().toFixed(2));
